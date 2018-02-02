f7ff312d91b6d59bbfc9b318b47ec1eb5fcccbad
Mitt Coats <mittcoats@gmail.com>
2018-02-02 11:23:36 -0800

    add programmatic routing

---
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+
+@Component({
+  selector: 'app-recipe-edit',
+  templateUrl: './recipe-edit.component.html',
+  styleUrls: ['./recipe-edit.component.css']
+})
+export class RecipeEditComponent implements OnInit {
+  id: number;
+  editMode = false;
+
+  constructor(private route: ActivatedRoute) { }
+
+  // For observables like Params, Angular handles clean up
+  // However, customer observables require clean up
+
+  ngOnInit() {
+    this.route.params
+      .subscribe(
+        (params: Params) => {
+          this.id = +params['id'];
+          this.editMode = params['id'] != null;
+          // console.log(this.editMode);
+        }
+      );
+  }
+
+}
